c59f45aceb89ba2b669b8328c62fe291da37f938
lmq <72318911+meetox80@users.noreply.github.com>
2024-07-28 12:45:55 +0100

    init

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace zstio_tv
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
